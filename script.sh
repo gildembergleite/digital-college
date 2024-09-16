48d175ce8bf226e006d4f689c2ef43939f3d3a78
gildembergleite <gildembergleite@gmail.com>
2024-09-16 20:57:56 -0300

    feat: created structure for this atividades

--- script.sh
+++ script.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Diretório principal
-mkdir -p ./conteudo
+# Diretórios
+mkdir -p ./conteudos
 mkdir -p ./desafios
+mkdir -p ./atividades
 
 # Criação das pastas para cada aula (48 aulas)
 for i in {1..48}; do
   # Pasta para aulas
-  mkdir -p ./conteudo/aula_$i
-  touch ./conteudo/aula_$i/README.md
+  mkdir -p ./conteudos/aula_$i
+  touch ./conteudos/aula_$i/README.md
   
   # Pasta para desafios
   mkdir -p ./desafios/aula_$i
   touch ./desafios/aula_$i/README.md
+
+  # Pasta para atividades
+  mkdir -p ./atividades/aula_$i
+  touch ./atividades/aula_$i/README.md
 done
 
 echo "Estrutura de pastas criada com sucesso!"
